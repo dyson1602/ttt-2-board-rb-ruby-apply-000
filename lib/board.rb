8076b2a1f2eacb5a59934eac22100b9fb282962b
Christopher Clark <cclark000@citymail.cuny.edu>
2020-08-20 20:42:04 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 # Define the variable board below.
 
+# This method of populating the array 'board' is incredibly clunky, but the course hasn't given instruction on loops yet, so I am trying to be as literal as possible for the time being.
+
 board = []
 
 board << " "
